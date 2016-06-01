e377b6fa5a6f5daa8e64bdfc67e2c3ab4f2e09d1
Agnieszka <jessamine90@gmail.com>
2016-06-01 16:39:43 +0200

    init - starting with libraries

---
+++ app/main.ts
@@ -0,0 +1,6 @@
+import {bootstrap}    from '@angular/platform-browser-dynamic';
+import {AppComponent} from './app.component';
+import * as _ from 'underscore';
+import * as duScroll from 'angular-scroll';
+
+bootstrap(AppComponent);
