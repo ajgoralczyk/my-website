32e6601204bec3b4d4c772cfb43c1ec420642a69
Agnieszka <jessamine90@gmail.com>
2016-06-14 14:17:14 +0200

    server.js config

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
-import express = require('express');
-import path = require('path');
-var port: number = process.env.PORT || 3000;
-var app = express();
+var express = require('express'),  
+    path = require('path'),
+    fs = require('fs');
 
-var server = app.listen(port, function() {
-    var host = server.address().address;
-    var port = server.address().port;
-    console.log('This express app is listening on port:' + port);
+var app = express();  
+var staticRoot = __dirname + '/';
+
+app.set('port', (process.env.PORT || 3000));
+
+app.use(express.static(staticRoot));
+
+app.use(function(req, res, next){
+
+    // if the request is not html then move along
+    var accept = req.accepts('html', 'json', 'xml');
+    if(accept !== 'html'){
+        return next();
+    }
+
+    // if the request has a '.' assume that it's for a file, move along
+    var ext = path.extname(req.path);
+    if (ext !== ''){
+        return next();
+    }
+
+    fs.createReadStream(staticRoot + 'index.html').pipe(res);
+
+});
+
+app.listen(app.get('port'), function() {  
+    console.log('app running on port', app.get('port'));
 });
