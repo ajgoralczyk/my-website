5eb3328fc18405117766ad63119aa6d887741310
Agnieszka <jessamine90@gmail.com>
2016-06-19 20:25:22 +0200

    angular version change

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 var express = require('express'),  
     path = require('path'),
-    fs = require('fs');
+    fs = require('fs'),
+    sass = require('node-sass');
+
 
 var app = express();  
 var staticRoot = __dirname + '/';
 
 app.set('port', (process.env.PORT || 3000));
+
+app.use(
+     sass.middleware({
+         src: __dirname + '/scss',
+         dest: __dirname + '/app/css',
+         debug: true
+     })
+ );
 
 app.use(express.static(staticRoot));
 
@@ -16,4 +26,3 @@ app.use(function(req, res, next){
 app.listen(app.get('port'), function() {  
     console.log('app running on port', app.get('port'));
 });
-
