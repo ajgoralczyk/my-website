fa3b56c763537dec10aa709eb2f3bd5488049dcf
Agnieszka <jessamine90@gmail.com>
2016-06-23 23:33:00 +0200

    server on heroku - another try

--- server.js
+++ server.js
@@ -1,20 +1,12 @@
 var express = require('express'),  
     path = require('path'),
-    fs = require('fs'),
-    sassMiddleware = require('node-sass-middleware');
+    fs = require('fs');
 
 var app = express();  
 var staticRoot = __dirname + '/';
 
 app.set('port', (process.env.PORT || 3000));
 
-app.use(sassMiddleware({
-    src: __dirname + '/scss',
-    dest: path.join(__dirname, 'css'),
-    debug: true,
-    outputStyle: 'compressed',
-    prefix:  '/css'  // Where prefix is at <link rel="stylesheets" href="prefix/style.css"/>
-}));
 
 app.use(express.static(staticRoot));
 
